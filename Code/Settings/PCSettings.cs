f2043d74b3ad5926f1d4b9e26ce463fd912dcfd4
River Mochi <97197236+River-Mochi@users.noreply.github.com>
2026-08-18 14:25:04 -0700

    tidy EOL

--- Code/Settings/PCSettings.cs
+++ Code/Settings/PCSettings.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="PCSettings.cs" company="River-Mochi">
+// <copyright file="PCSettings.cs" company="River-Mochi">
 // Copyright (c) 2026 River-Mochi. All rights reserved.
 // Licensed under the GNU General Public License v3.0 or later,
 // with the Cities: Skylines II Linking Exception.
@@ -209,9 +209,6 @@ namespace ParkingControl
 
             // Visibility-only setting changes must not rescan lanes.
             m_AppliedScope = m_ParkingScope;
-            if (m_ParkingScope != ParkingScope.Off)
-            {
-            }
 
             ParkingPolicySystem.RefreshVisibility();
             NoStreetParkingSystem.RequestReconcile();
